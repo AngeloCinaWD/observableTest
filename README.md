ce6234564ce20033dbbb7bf7a0919764dadc2f44
AngeloCinaWD <angelo.cina@yahoo.it>
2024-11-16 15:58:13 +0100

    rxjs operators

--- README.md
+++ README.md
@@ -1,27 +1,12 @@
 # ObservableTest
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.7.
+Progetto ng18, node 22.4.0  
+Creato progetto no standalone e senza test: ng new observableTest --no-standalone -S
 
-## Development server
+## [RxJS](https://rxjs.dev/)
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+## [RxJS Operators](https://rxjs.dev/guide/operators)
 
-## Code scaffolding
+## Descrizione dei metodi nelle reference di RxJS:
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+- [map operator](https://rxjs.dev/guide/operators)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'observableTest';
+
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    // salvo la sottoscrizione ad un observable in una costante, il subscribe attiva l'observable che altrimenti non emetterebbe nulla. Gli si può passare un oggetto osservatore con 3 proprietà con altrettante funzioni di callback
+    // next, error e complete. next qualcosa da fare per ogni valore emesso, error in caso di errore durante lo streaming, complete quando l'observable non emette più valori perchè ha finito
+    // interval è una funzione implementata e fornita dalla libreria rxjs, uno dei tanti metodi per avere un observable, in questo caso già fatto: emette un valore numerico ogni tot di tempo, determinabile in millisecondi
+
+    // const subscription = interval(1000).subscribe({
+    //   next: (val) => console.log(val),
+    // });
+
+    // OPERATORI RXJS
+    // si aggiungono prima della sottoscrizione tramite metodo .pipe() (non deve essere importato, mentre gli operatori utilizzati)
+    // gli operatori sono metodi richiamati come argomenti del metodo pipe
+    // uso operatore map, funzione callback come argomento applicata ad ogni valore emesso dall'observable, esempio moltilplico per 2 ogni valore, riuso di nuovo map e moltiplico il valore ottenuto per 3, naturalmente posso concatenare altri operatori
+    const subscription = interval(1000)
+      .pipe(
+        map((val) => val * 2),
+        map((val) => val * 3)
+      )
+      .subscribe({
+        next: (val) => console.log(val),
+      });
+
+    this.destroyRef.onDestroy(() => {
+      console.log('distruggo tutto');
+    });
+  }
 }
