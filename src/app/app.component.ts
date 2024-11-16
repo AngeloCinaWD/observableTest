7fd6c7fbdecdd0e2ff6313146191dc9f5bdca79f
AngeloCinaWD <angelo.cina@yahoo.it>
2024-11-16 20:14:23 +0100

    Converting Signals To Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, DestroyRef, effect, inject, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -20,6 +27,10 @@ export class AppComponent {
   // creo una proprietà che è un signal con valore iniziale 0
   clickCount = signal(0);
   clickCount2 = signal(0);
+
+  // angular mette a disposizione anche il metodo computed() che permette di fare qualcosa ogni volta che il signal indicato al suo interno cambia
+  // inquesto caso ad ogni click del button relativo a clickCount si aggiornerà anche questo valore mostrando il valore di clickCount moltiplicato 2
+  doubleCount = computed(() => this.clickCount() * 2);
 
   ngOnInit(): void {
     // salvo la sottoscrizione ad un observable in una costante, il subscribe attiva l'observable che altrimenti non emetterebbe nulla. Gli si può passare un oggetto osservatore con 3 proprietà con altrettante funzioni di callback
@@ -58,4 +69,9 @@ export class AppComponent {
   onClick2() {
     this.clickCount2.update((valorePrecedente) => valorePrecedente + 1);
   }
+
+  // tutto quello che possiamo fare con le nuove features di angular (signals, computed etc) ci permette di non utilizzare la libreria RxJS con gli observables. Utilizzare RxJS è in alcuni casi molto meno complesso e con meno codice da scrivere.
+  // la differenza principale sta nel fatto che gli observables sono un flusso di dati nel tempo che parte nel momento in cui c'è un subscribers, mentre i signals sono dei contenitori di valori di cui è possibile leggerne il contenuto in qualsiasi momento, senza bisogno di doversi iscrivere
+  // questo rende gli observables perfetti per events o qualsiasi altra cosa del genere dove i valori arrivano in modo asincrono
+  // i signals sono invece perfetti per la gestione dello state dell'app, quindi dati fissi all'inizio e che poi otrebbero cambiare nel tempo ed essere riflessi nell'interfaccia utente
 }
